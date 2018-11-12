2c91910b76ca00d6f241a0a46429a1b5482805db
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2018-11-12 11:10:18 +0100

    provide custom require() method. Please avoid its use....

--- source/bundleIndex.js
+++ source/bundleIndex.js
@@ -1,34 +1,31 @@
 /**
  * Client dependencies injected via browserify
  */
-self.webpackData = __webpack_require__
-import "webpack-runtime-require";
+
 import work from 'webworkify-webpack'
 import jsWorkerUrl from "url-loader!ace-builds/src-noconflict/worker-javascript.js";
 import jsonWorkerUrl from "url-loader!ace-builds/src-noconflict/worker-json.js";
 import htmlWorkerUrl from "url-loader!ace-builds/src-noconflict/worker-html.js";
 
-self.require      = self.Require;
-
-self.async        = require('async');
-self._            = require('lodash');
-self.EventEmitter = require('events');
-self.Heap         = require('heap');
-self.jsSHA        = require("jssha");
-self.kdTree       = require('kdt');
-self.numeric      = require('numeric');
-self.prettyData   = require('pretty-data').pd;
-self.randomJS     = require('random-js');
-self.THREE        =	require('three');
+self.async            = require('async');
+self._ = self.lodash  = require('lodash');
+self.EventEmitter     = require('events');
+self.heap = self.Heap = require('heap');
+self.jsSHA            = require("jssha");
+self.kdTree           = require('kdt');
+self.numeric          = require('numeric');
+self.prettyData       = require('pretty-data').pd;
+self.randomJS         = require('random-js');
+self.THREE            =	require('three');
 	require('three/examples/js/controls/TransformControls.js');
 	require('./ext/VTKLoader.js');
 	require('./ext/TrackballControls2.js');
 	require('./ext/CTMLoader.js');
 
-require('bluebird');
-require('chalk');
-require('jstat');
-require('ttest');
+self.bluebird = self.Promise = require('bluebird');
+self.chalk            = require('chalk');
+self.jstat            = require('jstat');
+self.ttest            = require('ttest');
 require('./ext/mhdParse.js');
 
 if (typeof importScripts !== 'function') {
@@ -49,12 +46,12 @@ if (typeof importScripts !== 'function') {
 		+ (getCookie("homeURL") || self.location.pathname)
 		+ '/');
 
-	self.io        = require('socket.io-client');
-	self.d3		  = require ('d3');
-	self.c3        = require ('c3');
+	self.io = require('socket.io-client');
+	self.d3	= require ('d3');
+	self.c3 = require ('c3');
 		require ('c3/c3.css');
 
-	self.ace   = require('ace-builds/src-noconflict/ace');
+	self.ace = require('ace-builds/src-noconflict/ace');
 	self.ace.config.setModuleUrl( "ace/mode/javascript_worker", jsWorkerUrl );
 	self.ace.config.setModuleUrl( "ace/mode/json_worker", jsonWorkerUrl );
 	self.ace.config.setModuleUrl( "ace/mode/html_worker", htmlWorkerUrl );
@@ -73,6 +70,12 @@ if (typeof importScripts !== 'function') {
 
 	THREE.VTKLoader.prototype.createWorker = function () {
 		return work(require.resolve('./ext/VTKWorker.js'), { all : true } );
+	}
+
+	self.require = function (module) {
+		if (module === 'desk-client' ) return self.desk;
+		if ( self[ module ] ) return self[ module ];
+		throw new Error( 'module ' + module + ' not found!' ).stack;
 	}
 }
 
