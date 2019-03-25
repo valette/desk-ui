13a95de2fc87c37472335b8d06c0e24e4f427e31
AGIER Remi <remi@aigen.fr>
2019-03-25 17:16:23 +0100

    update build & readme

--- build.sh
+++ build.sh
@@ -10,12 +10,16 @@ cp source/script/STLLoader.js build/script/STLLoader.js
 cp -r source/resource/ build
 python generate.py build
 
-#Create flat build (folder)
+cd build
+npm install
+cd ..
+
+#Create flat build (folder : win32, win64, linux64, mac64)
 electron-packager --overwrite --icon=icon.ico --azar=true --app-version=2.0.0 --arch ia32 --platform win32 build/ EduAnat2
 electron-packager --overwrite --icon=icon.ico --azar=true --app-version=2.0.0 --arch ia32 --platform linux build/ EduAnat2
 electron-packager --overwrite --icon=icon.ico --azar=true --app-version=2.0.0 --arch x64 --platform linux build/ EduAnat2
 electron-packager --overwrite --icon=icon.ico --azar=true --app-version=2.0.0 --arch x64 --platform darwin build/ EduAnat2
 for i in ./EduAnat2-*; do zip -rqy "dist/${i%/}.zip" "$i"; done
 
-#Create packaged build
+#Create packaged build (win64 : .exe installer, linux : .deb)
 electron-builder build -wl --config builder-effective-config.yaml
