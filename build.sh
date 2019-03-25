c304680c9bc8083f03c83f451a9acc3fd9c1c517
Remi AGIER <remi@aigen.fr>
2019-03-25 12:59:59 +0100

    update build

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+npm install
+
 rm -rf EduAnat2-*
 
 node source/build-worker.js
@@ -8,18 +10,22 @@ cp source/script/test.js build/script/test.js
 cp source/script/bundle.js build/script/bundle.js
 cp source/script/STLLoader.js build/script/STLLoader.js
 cp -r source/resource/ build
-python generate.py build
+
+#browserify (without -g npm install)
+node node_modules/browserify/bin/cmd.js source/browserified.js -r async -r heap -r jstat -r lodash -r operative -r ./source/desk-client.js:desk-client > source/script/bundle.js
+
+python generate.py source && python generate.py build
 
 cd build
 npm install
 cd ..
 
 #Create flat build (folder : win32, win64, linux64, mac64)
-electron-packager --overwrite --icon=icon.ico --azar=true --app-version=2.0.0 --arch ia32 --platform win32 build/ EduAnat2
-electron-packager --overwrite --icon=icon.ico --azar=true --app-version=2.0.0 --arch ia32 --platform linux build/ EduAnat2
-electron-packager --overwrite --icon=icon.ico --azar=true --app-version=2.0.0 --arch x64 --platform linux build/ EduAnat2
-electron-packager --overwrite --icon=icon.ico --azar=true --app-version=2.0.0 --arch x64 --platform darwin build/ EduAnat2
+node node_modules/electron-packager/cli.js --overwrite --icon=icon.ico --azar=true --app-version=2.0.0 --arch ia32 --platform win32 build/ EduAnat2
+node node_modules/electron-packager/cli.js --overwrite --icon=icon.ico --azar=true --app-version=2.0.0 --arch ia32 --platform linux build/ EduAnat2
+node node_modules/electron-packager/cli.js --overwrite --icon=icon.ico --azar=true --app-version=2.0.0 --arch x64 --platform linux build/ EduAnat2
+node node_modules/electron-packager/cli.js --overwrite --icon=icon.ico --azar=true --app-version=2.0.0 --arch x64 --platform darwin build/ EduAnat2
 for i in ./EduAnat2-*; do zip -rqy "dist/${i%/}.zip" "$i"; done
 
 #Create packaged build (win64 : .exe installer, linux : .deb)
-electron-builder build -wl --config builder-effective-config.yaml
+node node_modules/electron-builder/out/cli/cli.js build -wl --config builder-effective-config.yaml
