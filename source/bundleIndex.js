479c26a824556dbe808764b71977b39d6364083e
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2020-09-25 10:18:29 +0200

    load THREE.STLLoader() and THREE.TransformControls() as modules

--- source/bundleIndex.js
+++ source/bundleIndex.js
@@ -1,3 +1,5 @@
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
+import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js';
 
 self.async            = require('async');
 self._ = self.lodash  = require('lodash');
@@ -8,11 +10,12 @@ self.kdTree           = require('kdt');
 self.numeric          = require('numeric');
 self.randomJS         = require('random-js');
 self.THREE            =	require('three');
-	require('three/examples/js/controls/TransformControls.js');
-	require('three/examples/js/loaders/STLLoader.js');
 	require('./ext/CTMLoader.js');
 	require('./ext/VTKLoader.js');
 	require('./ext/TrackballControls2.js');
+
+self.THREE.STLLoader = STLLoader;
+self.THREE.TransformControls = TransformControls;
 
 self.bluebird = self.Promise = require('bluebird');
 self.chalk            = require('chalk');
