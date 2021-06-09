43615142343ccee978552f77d02bf4d65ed730b0
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2021-06-09 15:44:08 +0200

    added link to qooxdoo demo browser

--- source/class/desk/Actions.js
+++ source/class/desk/Actions.js
@@ -364,6 +364,7 @@ qx.Class.define("desk.Actions",
 				'desk-ui API' : desk.FileSystem.getFileURL('ui/compiled/dist/apiviewer'),
 				'desk-ui debug mode' : desk.FileSystem.getFileURL('ui/compiled/source'),
 				'desk-ui changelog' : 'https://github.com/valette/desk-ui/commits/master',
+				'Qooxdoo demo browser' : 'http://qooxdoo.org/qxl.demobrowser/',
 				'Qooxdoo Widget browser' : 'https://qooxdoo.org/qxl.widgetbrowser/',
 				'Qooxdoo Interactive playground' : 'https://qooxdoo.org/qxl.playground/'
 			};
