1102df9eb0818386ed5155f2a26e3b01c537b658
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2022-10-07 10:33:30 +0200

    desk.WindowMixin : only set window size when content size is not set

--- source/class/desk/WindowMixin.js
+++ source/class/desk/WindowMixin.js
@@ -4,11 +4,16 @@
 qx.Mixin.define("desk.WindowMixin",
 {
 	construct : function () {
-		var minSize = Math.round(Math.min(window.innerWidth, window.innerHeight) * 0.85);
 
 		var win = new qx.ui.window.Window();
-		win.set({layout : new qx.ui.layout.HBox(), showMinimize : false,
-			width : minSize, height : minSize});
+		win.set( { layout : new qx.ui.layout.HBox(), showMinimize : false } );
+
+		if ( !this.getHeight() || !this.getWidth() ) {
+
+			const minSize = Math.round(Math.min(window.innerWidth, window.innerHeight) * 0.85);
+			win.set({ width : minSize, height : minSize});
+
+		}
 
         win.add(this, {flex : 1});
         win.open();
