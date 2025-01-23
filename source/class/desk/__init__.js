bc71c3aeb9e0af8a45b649b244ec3839ae039d2a
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2025-01-23 15:27:34 +0100

    more doc

--- source/class/desk/__init__.js
+++ source/class/desk/__init__.js
@@ -1,6 +1,6 @@
 /** <h3> desk API Documentation </h3>
  *
- * Set of classses to expose server files, allow remote execution of 
+ * Set of classes to expose server files, allow remote execution of 
  * command-line programs
  *
  */
---
+++ source/class/desk/core/__init__.js
@@ -0,0 +1,5 @@
+/** <h3> desk core Documentation </h3>
+ *
+ * Set of classes to include external libraries such as lodash, THREE.js
+ *
+ */
